2cae3ecd4d7159678377d6e34b0d206c2d98b082
Alan Jones <alanjones83@gmail.com>
2017-10-23 16:04:38 -0500

    Update js-review.js

--- js-review.js
+++ js-review.js
@@ -105,6 +105,29 @@ var t = new Triangle(1,2,3);
 
 /********************************************************************************************************************** */
 
+// PROTOTYPE INHERITANCE SIMPLE
+
+var myCar = {
+ 
+  name: "Ford Escort",
+ 
+  drive: function () {
+    console.log( "Weeee. I'm driving!" );
+  },
+ 
+  panic: function () {
+    console.log( "Wait. How do you stop this thing?" );
+  }
+ 
+};
+ 
+// Use Object.create to instantiate a new car
+var yourCar = Object.create( myCar );
+ 
+// Now we can see that one is a prototype of the other
+console.log( yourCar.name );
+
+
 
 // PROTOTYPE CHAINING EXAMPLE 
 
